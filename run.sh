9c90da2b84a89508781985934a6a4da750eae928
ultrons <singh.vaibhav.83@gmail.com>
2017-05-31 10:53:09 +0000

    DMN+ config

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@ python main.py \
        --phase=train \
        --cnn_model=vgg16 \
        --cnn_model_file=./tfmodels/vgg16.tfmodel \
-       --load_cnn_model
+       --load_cnn_model \
+       --attention=gru \
+       --init_embed_with_glove  | tee run_$$.log
+
         
        
